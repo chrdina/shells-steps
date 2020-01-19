21ecce7afd9046d4febfca243faa6d0a6f510562
Christian Hrdina <christianhrdina@192-168-1-12.tpgi.com.au>
2020-01-19 23:50:28 +1100

    Trips displaying all content mapped from props -- messy

--- src/pages/trip.js
+++ src/pages/trip.js
@@ -2,10 +2,26 @@ import React from 'react';
 
 function Trip(props) {
 
-  const { params } = props.match
+  const { params } = props.match;
+
+  console.log("Trip Props: ");
+  console.log(props.location.data);
+
+  const data = props.location.data;
+  const propsList = Object.entries(data).map(([key,value]) => {
+    return (
+      <p>{key}: {value.toString()}</p>
+    )
+  });
+
+  // const tripDataMapped = Object.entries(params)
   return (
     <React.Fragment>
       <h1>Trip ID is: {params.id}</h1>
+      <div class="trip-hero-img">
+        <img src={data.tilePicTrip.fields.file.url} />
+      </div>
+      {propsList}
     </React.Fragment>
   );
 
