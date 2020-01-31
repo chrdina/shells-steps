eb5dd7d8dbfc6723bab248fdccd54953bd7c33c3
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-01-31 15:25:34 +1100

    Basic markdown format displayed on Trip pages

--- src/pages/trip.js
+++ src/pages/trip.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 function Trip(props) {
 
@@ -8,21 +9,38 @@ function Trip(props) {
   console.log(props.location.data);
 
   const data = props.location.data;
-  const propsList = Object.entries(data).map(([key,value]) => {
+
+  function markdown(input) {
     return (
-      <p>{key}: {value.toString()}</p>
+      <ReactMarkdown>
+        {input}
+      </ReactMarkdown>
     )
-  });
+  }
+
+  function mapPropsToPara(data) {
+    const propsList = Object.entries(data).map(([key,value]) => {
+      return (
+        <p>{key}: {value.toString()}</p>
+      )
+    });
+    return propsList;
+  }
 
   // const tripDataMapped = Object.entries(params)
   return (
-    <React.Fragment>
-      <h1>Trip ID is: {params.id}</h1>
+    <div>
+      <h1>{params.id}</h1>
       <div class="trip-hero-img">
         <img src={data.tilePicTrip.fields.file.url} />
       </div>
-      {propsList}
-    </React.Fragment>
+      {markdown(data.tripName)}
+      {markdown(data.tripDate)}
+      {markdown(data.tripLocations)}
+      {markdown(data.highlights)}
+      {markdown(data.tripItinirary)}
+      {markdown(data.tripDetails)}
+    </div>
   );
 
 }
