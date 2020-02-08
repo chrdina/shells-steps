12463326981344bbdd47bbfd79ead66a5ab5da28
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-02-08 18:42:56 +1100

    Trip photos displaying inline and body margins added.

--- src/pages/trip.js
+++ src/pages/trip.js
@@ -10,21 +10,13 @@ function Trip(props) {
 
   const data = props.location.data;
 
-  // Used only for debugging
-  function mapPropsToPara(data) {
-    const propsList = Object.entries(data).map(([key,value]) => {
-      return (
-        <p>{key}: {value.toString()}</p>
-      )
-    });
-    return propsList;
-  }
-
   return (
     <>
       <h1>{params.id}</h1>
       <div class="trip-hero-img">
-        <img src={`${data.tilePicTrip.fields.file.url}?fm=jpg&fl=progressive`}/>
+        {data.tripPhotos && data.tripPhotos.map(
+          (image) => <img src={`${image.fields.file.url}?fm=jpg&fl=progressive`} />
+        )}
       </div>
       <ReactMarkdown>{data.tripName}</ReactMarkdown>
       <ReactMarkdown>{data.tripDate}</ReactMarkdown>
