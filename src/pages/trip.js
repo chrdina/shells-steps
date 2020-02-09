beb2fe876c1d72633c359cd8d83400116fddc82f
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-02-09 13:36:09 +1100

    Basic trip page layout completed. Some styling included. Image sizing needs work

--- src/pages/trip.js
+++ src/pages/trip.js
@@ -14,22 +14,38 @@ function Trip(props) {
 
   return (
     <>
-      <h1>{params.id}</h1>
+      <div class="header">{params.id} <hr /></div>
 
-        <Carousel arrows infinite centered>
-          {data.tripPhotos && data.tripPhotos.map(
-            (image, key) => <img src={`${image.fields.file.url}?fm=jpg&fl=progressive&h=500&w=700`} key={key} />
-          )}
-        </Carousel>
+      <div class="hero-area">
+        <div class="hero-area-text">
+          <ReactMarkdown>{data.tripName}</ReactMarkdown>
+          <ReactMarkdown>{data.tripDate}</ReactMarkdown>
+          <ReactMarkdown>{data.tripLocations}</ReactMarkdown>
+        </div>
+        <div class="hero-area-img">
+          <img src={`${data.tilePicTrip.fields.file.url}?fm=jpg&fl=progressive`} />
+        </div>
+      </div>
+
+      <div class="content-grid">
+        <div class="col">
+          <ReactMarkdown>{data.highlights}</ReactMarkdown>
+          <ReactMarkdown>{data.tripItinirary}</ReactMarkdown>
+          <ReactMarkdown>{data.tripDetails}</ReactMarkdown>
+        </div>
+        <div class="col">
+          <Carousel arrows infinite>
+            {data.tripPhotos && data.tripPhotos.map(
+              (image, key) => <img src={`${image.fields.file.url}?fm=jpg&fl=progressive&h=400&w=600`} key={key} />
+            )}
+          </Carousel>
+        </div>
+      </div>
 
 
 
-      <ReactMarkdown>{data.tripName}</ReactMarkdown>
-      <ReactMarkdown>{data.tripDate}</ReactMarkdown>
-      <ReactMarkdown>{data.tripLocations}</ReactMarkdown>
-      <ReactMarkdown>{data.highlights}</ReactMarkdown>
-      <ReactMarkdown>{data.tripItinirary}</ReactMarkdown>
-      <ReactMarkdown>{data.tripDetails}</ReactMarkdown>
+
+
     </>
   );
 
