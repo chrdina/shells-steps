82d7ada3352012b36ba3d0e16a226e6cd16233fe
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-03-25 19:58:57 +1100

    Removed duplicate dates from side anchor list

--- src/components/anchorNav.js
+++ src/components/anchorNav.js
@@ -8,13 +8,35 @@ function AnchorNav(props) {
   //     <li> <a href={`#${id}`}> {year} </a> </li>
   //   )}
   // </ul>
+  function removeDuplicates(array, propName) {
+    console.log("Removing duplicates for array: ");
+    console.log(array);
+    var arrayOut = [];
+    var i = 0;
+
+    while (i < array.length-1) {
+      arrayOut.push(array[i]);
+      if (i+1 < array.length-1) {
+        while (array[i][propName] == array[i+1][propName]) {
+          i++;
+          console.log("iterate...");
+        }
+      }
+      i++;
+    }
+    console.log("New array: ");
+    console.log(arrayOut);
+    return arrayOut;
+  }
+
+  const testArray = props.data && removeDuplicates(props.data, "date");
 
   return (
 
     <div id="side-nav-left">
       Select date:
       <ul>
-        {props.data && props.data.map((listItem) =>
+        {props.data && removeDuplicates(props.data, "date").map((listItem) =>
           <li> <a href={`#${listItem.name}`}> {listItem.date} </a> </li>
         )}
       </ul>
