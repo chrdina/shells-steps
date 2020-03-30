527c032aa5d748cab0875d19eb22eae5d463c949
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-03-30 19:52:44 +1100

    Updates

---
+++ precache-manifest.57069ba99ad1a29957f80be0c887ae07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50128a6a5cbb478f91007d297fa9e140",
+    "url": "/wheres-shell/index.html"
+  },
+  {
+    "revision": "5781027a2f8ce346665c",
+    "url": "/wheres-shell/static/css/2.15deb5ce.chunk.css"
+  },
+  {
+    "revision": "2dc3d0de0e8cebbc034c",
+    "url": "/wheres-shell/static/css/main.32ae82ba.chunk.css"
+  },
+  {
+    "revision": "5781027a2f8ce346665c",
+    "url": "/wheres-shell/static/js/2.138f0dc0.chunk.js"
+  },
+  {
+    "revision": "707a7ec227a0e1b27916aaef2a971abd",
+    "url": "/wheres-shell/static/js/2.138f0dc0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dc3d0de0e8cebbc034c",
+    "url": "/wheres-shell/static/js/main.28a1a950.chunk.js"
+  },
+  {
+    "revision": "6bf5d9d8467390638532",
+    "url": "/wheres-shell/static/js/runtime-main.2db50fd5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wheres-shell/precache-manifest.0422037d5e5a175375a15ca4830a3058.js"
+  "/wheres-shell/precache-manifest.57069ba99ad1a29957f80be0c887ae07.js"
 );
 
 self.addEventListener('message', (event) => {
