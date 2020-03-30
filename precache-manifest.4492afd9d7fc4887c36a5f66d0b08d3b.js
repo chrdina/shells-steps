4d53249692fc3c0ba875dedef6af7bf40c8ee814
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-03-30 13:43:25 +1100

    Updates

---
+++ precache-manifest.4492afd9d7fc4887c36a5f66d0b08d3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115a3f58f3c8fb112df99a1907ec766a",
+    "url": "/wheres-shell/index.html"
+  },
+  {
+    "revision": "0839999f455e0e0dc690",
+    "url": "/wheres-shell/static/css/2.15deb5ce.chunk.css"
+  },
+  {
+    "revision": "9bf27c3c6932e4d44aaa",
+    "url": "/wheres-shell/static/css/main.4ebbfd08.chunk.css"
+  },
+  {
+    "revision": "0839999f455e0e0dc690",
+    "url": "/wheres-shell/static/js/2.2b33b089.chunk.js"
+  },
+  {
+    "revision": "707a7ec227a0e1b27916aaef2a971abd",
+    "url": "/wheres-shell/static/js/2.2b33b089.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bf27c3c6932e4d44aaa",
+    "url": "/wheres-shell/static/js/main.0ee51a0f.chunk.js"
+  },
+  {
+    "revision": "6bf5d9d8467390638532",
+    "url": "/wheres-shell/static/js/runtime-main.2db50fd5.js"
+  }
+]);
