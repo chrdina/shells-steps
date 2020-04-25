1527841108f48e44fc0f98d1f13c4723121968f9
Christian Hrdina <christianhrdina@Christians-MacBook-Pro.local>
2020-04-25 23:51:09 +1000

    Fixed bug where month would incorrectly display as the next consecutive month in the year

--- src/dateFormatter.js
+++ src/dateFormatter.js
@@ -1,6 +1,6 @@
 function getDate (input, monthLength) { // input will be 2020-02-28 format
   const inputArray = input.split("-");
-  const date = new Date(inputArray[0], inputArray[1], inputArray[2]);
+  const date = new Date(inputArray[0], inputArray[1]-1, inputArray[2]);
 
   const day = date.getDate().toString();
   const month = date.toLocaleString('default', { month: monthLength });
