1d1c3274d2bbcd12c264e9eb3ef8337dc8da6345
chrdina <christian_hrdina@hotmail.com>
2020-09-29 16:24:07 +1000

    Refactor trips page to be a functional component

--- src/pages/trips.js
+++ src/pages/trips.js
@@ -1,84 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tile from "../components/tile";
 import client from "../contentfulProvider";
 import AnchorNav from "../components/anchorNav";
-import { HashLink as Link } from "react-router-hash-link";
 
-class Trips extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDateSelect = this.handleDateSelect.bind(this);
-    this.state = {
-      data: [],
-      selectedYear: "",
-      filterActive: false,
-    };
-  }
+const Trips = () => {
 
-  componentDidMount() {
-    client
-      .getEntries({ content_type: "Trip", order: "-fields.tripDate" })
-      .then((response) => this.setState({ data: response.items }));
-  }
+  const [selectedYear, setSelectedYear] = useState();
+  const [filterActive, setFilterActive] = useState();
+  const [data, setData] = useState();
 
-  handleDateSelect(year) {
-    if (year === this.state.selectedYear) {
-      this.setState({ selectedYear: "" });
-      this.setState({ filterActive: false });
+  useEffect(() => {
+    const handleFetchData = async () => {
+      const response = await client.getEntries({ content_type: "Trip", order: "-fields.tripDate" });
+      setData(response.items);
+    }
+    if (data == null) {
+      handleFetchData();
+    }
+  }, [])
+
+  const handleDateSelect = (year) => {
+    if (year === selectedYear) {
+      setSelectedYear("");
+      setFilterActive(false);
     } else {
-      this.setState({ selectedYear: year });
-      this.setState({ filterActive: true });
+      setSelectedYear(year);
+      setFilterActive(true);
     }
   }
 
-  render() {
-    const selectedYear = this.state.selectedYear;
-    const filterActive = this.state.filterActive;
+  return (
+    <>
+      {data && (
+        <AnchorNav
+          data={data}
+          selectedYear={selectedYear}
+          onDateSelect={handleDateSelect}
+        />
+      )}
 
-    return (
-      <>
-        {this.state.data && (
-          <AnchorNav
-            data={this.state.data}
-            selectedYear={selectedYear}
-            onDateSelect={this.handleDateSelect}
-          />
-        )}
-
-        <div className="content-container">
-          <div className="page-header">
-            <h1>Trips</h1>
-          </div>
-          <div className="tiles">
-            {this.state.data &&
-              this.state.data.map((trip) => (
-                <Tile
-                  key={trip.sys.id}
-                  id={trip.sys.id}
-                  to={`/trips/${trip.sys.id}`}
-                  text={trip.fields.tripName}
-                  imgSrc={
-                    trip.fields.tilePicTrip &&
-                    trip.fields.tilePicTrip.fields !== null
-                      ? trip.fields.tilePicTrip.fields.file.url
-                      : undefined
-                  }
-                  filteredOut={
-                    filterActive &&
-                    selectedYear !== trip.fields.tripDate.split("-")[0]
-                  }
-                />
-              ))}
-          </div>
+      <div className="content-container">
+        <div className="page-header">
+          <h1>Trips</h1>
         </div>
+        <div className="tiles">
+          {data &&
+            data.map((trip) => (
+              <Tile
+                key={trip.sys.id}
+                id={trip.sys.id}
+                to={`/trips/${trip.sys.id}`}
+                text={trip.fields.tripName}
+                imgSrc={
+                  trip.fields.tilePicTrip &&
+                  trip.fields.tilePicTrip.fields !== null
+                    ? trip.fields.tilePicTrip.fields.file.url
+                    : undefined
+                }
+                filteredOut={
+                  filterActive &&
+                  selectedYear !== trip.fields.tripDate.split("-")[0]
+                }
+              />
+            ))}
+        </div>
+      </div>
 
-        {/* <div id="footer">
-          <Link smooth to="#top">
-            Back to top
-          </Link>
-        </div> */}
-      </>
-    );
-  }
+    </>
+  );
 }
 export default Trips;
