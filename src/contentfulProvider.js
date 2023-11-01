7169ecfae1ad77956c1802a61a82b583cc7b88f9
cHrdina <15863753+cHrdina@users.noreply.github.com>
2023-11-01 23:38:05 +1100

    rename env var

--- src/contentfulProvider.js
+++ src/contentfulProvider.js
@@ -2,7 +2,7 @@ var contentful = require("contentful");
 
 var client = contentful.createClient({
   space: "r1wogzr4p3xm",
-  accessToken: `${process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN}`,
+  accessToken: `${process.env.REACT_APP_API_KEY}`,
 });
 
 export default client;
